0c716661acfa9efdac7d4945a11333de858fc284
jatin0801 <2018.jatin.chhabria@ves.ac.in>
2021-04-14 15:22:27 +0530

    update

--- teproject/teproject/urls.py
+++ teproject/teproject/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from user import views as user_view
 from django.contrib.auth import views as auth
- 
+from user import views
+
 urlpatterns = [
  
     path('admin/', admin.site.urls),
@@ -27,5 +28,6 @@ urlpatterns = [
     path('login/', user_view.Login, name ='login'),
     path('logout/', auth.LogoutView.as_view(template_name ='user/index.html'), name ='logout'),
     path('register/', user_view.register, name ='register'),
- 
+    path('result/', user_view.result, name='result'),
+    
 ]
--- teproject/user/views.py
+++ teproject/user/views.py
@@ -9,7 +9,7 @@ from django.core.mail import EmailMultiAlternatives
 from django.template.loader import get_template
 from django.template import Context
 import os
-
+import joblib
   
 #################### index#######################################
 def index(request):
@@ -55,3 +55,39 @@ def Login(request):
             messages.info(request, f'account done not exit plz sign in')
     form = AuthenticationForm()
     return render(request, 'user/login.html', {'form':form, 'title':'log in'})
+
+################ import ml###################################################
+
+def result(request):
+    return render(request,"user/result.html")
+
+# def getPredictions(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q):
+#     import pickle
+#     model = joblib.load('xgboost.sav')
+#     scaled = joblib.load('encoder.sav')
+#     prediction = model.predict(le.transform([[a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q]]))
+#     print(prediction)
+
+# def result(request):
+#     a = int(request.GET['a'])
+#     b = int(request.GET['b'])
+#     c = int(request.GET['c'])
+#     d = int(request.GET['d'])
+#     e = int(request.GET['e'])
+#     f = int(request.GET['f'])
+#     g = int(request.GET['g'])
+#     h = int(request.GET['h'])
+#     i = int(request.GET['i'])
+#     j = int(request.GET['j'])
+#     k = int(request.GET['k'])
+#     l = int(request.GET['l'])
+#     m = int(request.GET['m'])
+#     n = int(request.GET['n'])
+#     o = int(request.GET['o'])
+#     p = int(request.GET['p'])
+#     q = int(request.GET['q'])
+
+
+#     result = getPredictions(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q)
+
+#     return render(request, 'result.html', {'result':ans})
